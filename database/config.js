e58668eb1d92e50a3a4059a1aef2ef0e2bc116ea
dimark <zhoudi@33.cn>
2020-04-17 21:42:29 +0800

    链接到数据库

--- database/config.js
+++ database/config.js
@@ -1,8 +1,8 @@
 const config = {
-    host: 'localhost', //主机名
-    database: 'lg', //使用的哪个数据库名
+    host: '172.16.101.61', //主机名
+    database: 'zhoudi', //使用的哪个数据库名
     username: 'root',//账号
-    password: '123456',//密码
+    password: 'chengshubei123',//密码
     port: 3306, //端口号，mysql默认3306
 }
 
